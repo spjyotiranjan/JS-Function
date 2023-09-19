2c16f7c853a2d86f22b4d876162b4447480738ee
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-09-19 15:10:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS-Function
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-bamwqm)
---
+++ script.js
@@ -0,0 +1,22 @@
+//Challenge 1
+// Step 1: Create a function with the function name as ageCalculator and pass two parameters, first one is birthYear and the second one is the presentYear.
+//Step 2: Pass two arguments as birthYear and presentYear while calling the function.
+//Step 3: Calculate the possible age and return it.
+//Step 4: Display the age on the console.
+// Your code goes here
+
+//Challenge 2: Following is the code snippet commented , remove the comments and get started with the challenge. Find the errors and rewrite the function.
+
+//Function Addition(){
+//let a = a+b;
+//return a;
+//console.log(a);
+//}
+//Addition(2,3)
+
+//Challenge 3:
+// Step 1: Declare the variable "x" with "var" keyword and store a string value "I can access it."
+//Step 2: Console log the value above the declaration of x variable.
+//Step 3: Console log the value after the decalaration of x.
+
+//Challenge 4: Create an arrow function for adding two numbers. Console log the value.
